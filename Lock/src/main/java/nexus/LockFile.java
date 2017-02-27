bea905f8e1d16a36cf3ad40d79574be2e015220b
angelless <dongkyu91622@gmail.com>
2017-02-28 04:17:07 +0900

    to String parameter-Position

--- Lock/src/main/java/nexus/LockFile.java
+++ Lock/src/main/java/nexus/LockFile.java
@@ -6,6 +6,13 @@ pubtlic static LockFile getInstance(){
 return instance;
 }
 public LockFile(Main plugin){
-
+  plugin.getConfig();
 }
+  
+  
+  public String toString(Position pos){
+    StringBuilder str = new StringBuilder();
+    str.append(pos.getFloorX+":").append(pos.getFloorY()+":").append(pos.getFloorZ()+":").append(pos.getLevel().getFolderName());
+    return str.toString();
+  }
 }
